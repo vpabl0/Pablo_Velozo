d3db76960d0b974d62d29088897a827ccdfd14df
Pablo Velozo <pabloestebanvelozo@gmail.com>
2024-06-10 20:19:00 -0300

    Delete TP1_Algoritmos/TP1_Velozo_Pablo directory

--- TP1_Algoritmos/TP1_Velozo_Pablo/ejercicio_1.js
+++
@@ -1,2 +0,0 @@
-document.write("Hola Mundo!");
-//Esta linea de codigo muestra en pantalla el texto "Hola Mundo!"
--- TP1_Algoritmos/TP1_Velozo_Pablo/ejercicio_2.js
+++
@@ -1,8 +0,0 @@
-let A, B;
-A=9;
-B=7;
-resultado=A+B;
-alert (`La suma de A+B es: ${resultado}`);
-alert (`La diferencia de A-B es: ${A-B}`);
-alert (`La multiplicación de AxB es: ${A*B}`);
-alert (`La división de A/B es: ${resultado=A/B}`);
